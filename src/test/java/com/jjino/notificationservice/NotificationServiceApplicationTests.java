38b097af9053fd9ec75b7629267f86a01db00320
SON1205 <jjinoson1205@gmail.com>
2026-04-01 18:27:58 +0900

    init

---
+++ src/test/java/com/jjino/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjino.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
